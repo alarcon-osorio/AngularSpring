5cbfce1a98f3b23397cba7d14ae590e63f84bb48
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-07-29 20:37:47 -0500

    Sin Deploy - Isuue Bug Open

---
+++ CrudAngularSpring/backend/src/main/java/com/api/controller/PeliculaController.java
@@ -0,0 +1,24 @@
+package com.api.controller;
+
+import com.api.model.Pelicula;
+import com.api.service.PeliculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PeliculaController {
+
+    @Autowired
+    private PeliculaService peliculaService;
+
+    @GetMapping("/api/peliculas")
+    public ResponseEntity<List<Pelicula>> getPeliculas(){
+        List<Pelicula> list = peliculaService.list();
+        return ResponseEntity.ok().body(list);
+    }
+
+}
